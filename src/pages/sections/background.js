c3e388e4aac30bbd6cf703abde437dbf7a9e45f2
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-10 14:41:36 +0800

    moved main title and footer inside mainbackground

--- src/pages/sections/background.js
+++ src/pages/sections/background.js
@@ -1,11 +1,13 @@
-import bg from '@styles/modules/bg.module.scss'
-import def from '@styles/modules/home.module.scss'
+import bg from '@styles/modules/bg.module.scss';
 
 export function MainBackground() {
     return(
         <section className={bg.photoContainer}>
-            <div className={bg.backgroundPicture}></div>
-            <div className={bg.backgroundOverlay}></div>
+            <div className={bg.backgroundPicture}>
+                <div className={bg.backgroundOverlay}></div>
+                <p className={bg.mainTitle}>SENNA<br/>MIST STACKS<br/>CALCULATOR</p>
+                <p className={bg.footer}>Splash art © Riot Games. This is a fan project not affiliated with Riot Games.</p>
+            </div>
         </section>
     )
 } 
