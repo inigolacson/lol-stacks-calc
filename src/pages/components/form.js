fb8345ab932458e316d7710fac77b717fd52d485
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-11 14:09:49 +0800

    form design without logic connection

--- src/pages/components/form.js
+++ src/pages/components/form.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, use} from 'react';
+import form from '@/styles/modules/form.module.scss';
 
 export function SennaCalculator() {
     const [stacks, setStacks] = useState(0);
@@ -25,8 +26,34 @@ useEffect(() => {
 }, [stacks, itemcrit]);
 
 return (
-    <div>
-
-    </div>
+    <section className={form.formContainer}>
+        <div className={form.title}>
+            CALCULATE MIST STATS NOW
+        </div>
+        <section className={form.inputContainer}>
+        <input className={form.inputBoxes} placeholder='MIST STACKS'></input>
+        <input className={form.inputBoxes} placeholder='CRITICAL STRIKE CHANCE FROM ITEMS'></input>
+        <button className={form.buttonBox}>Calculate</button>
+        </section>
+        <section className={form.resultContainer}>
+            <div className={form.itemContainer}>
+                <p className={form.label}>Attack Damage:</p>
+                <p className={form.result}>+{result.ad}.00%</p>
+            </div>
+            <div className={form.itemContainer}>
+                <p className={form.label}>Attack Range:</p>
+                <p className={form.result}>+{result.ad}.00%</p>
+            </div>
+            <div className={form.itemContainer}>
+                <p className={form.label}>Critical Strike Chance:</p>
+                <p className={form.result}>+{result.ad}%</p>
+            </div>
+            <div className={form.spacer}/>
+            <div className={form.itemContainer}>
+                <p className={form.label}>Life Steal:</p>
+                <p className={form.result}>+{result.ad}.00%</p>
+            </div>
+        </section>
+    </section>
 )
 }
