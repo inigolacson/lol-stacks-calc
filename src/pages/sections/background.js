2ba27dc320d81880b5409c3031909ad9c6a9fd9a
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-09-18 08:22:27 +0800

    feat: yay

--- src/pages/sections/background.js
+++ src/pages/sections/background.js
@@ -1,11 +1,14 @@
-import bg from '@styles/modules/bg.module.scss';
+import bg from "@styles/modules/bg.module.scss";
 
-export function MainBackground() {
-    return(
-        <section className={bg.photoContainer}>
-            <div className={bg.backgroundPicture}/>
-            <div className={bg.backgroundOverlay}/>
-            <p className={bg.footer}>Splash art © Riot Games. This is a fan project not affiliated with Riot Games.</p>     
-        </section>
-    )
-} 
+export default function MainBackground() {
+  return (
+    <section className={bg.photoContainer}>
+      <div className={bg.backgroundPicture} />
+      <div className={bg.backgroundOverlay} />
+      <p className={bg.footer}>
+        Splash art © Riot Games. This is a fan project not affiliated with Riot
+        Games.
+      </p>
+    </section>
+  );
+}
--- src/pages/sections/content.js
+++ src/pages/sections/content.js
@@ -1,14 +1,20 @@
-import form from '@styles/modules/content.module.scss';
-import bg from '@styles/modules/content.module.scss'
-import { SennaCalculator } from '../components/form';
+import form from "@styles/modules/content.module.scss";
+import bg from "@styles/modules/content.module.scss";
+import { SennaCalculator } from "../components/form";
 
-export function Content() {
-    return (
-        <section className={form.contentContainer}>
-                <div className={form.formContainer}>
-                <SennaCalculator/>
-                </div>
-                <p className={bg.mainTitle}>SENNA<br/>MIST STACKS<br/>CALCULATOR</p>
-        </section>
-    )
+export default function Content() {
+  return (
+    <section className={form.contentContainer}>
+      <div className={form.formContainer}>
+        <SennaCalculator />
+      </div>
+      <p className={bg.mainTitle}>
+        SENNA
+        <br />
+        MIST STACKS
+        <br />
+        CALCULATOR
+      </p>
+    </section>
+  );
 }
